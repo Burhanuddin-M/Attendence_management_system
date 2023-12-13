725f4c24be0e274d34c211947306e32ea93c516e
mazimez <asif987patel@gmail.com>
2023-12-14 00:52:23 +0530

    added: some seeders

--- database/factories/AttendanceFactory.php
+++ database/factories/AttendanceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Attendance;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,12 +17,18 @@ class AttendanceFactory extends Factory
      */
     public function definition(): array
     {
+        $has_extra_time = fake()->randomElement([1, 0]);
+        $extra_hours = 0;
+        $total_amount = 0;
+        if ($has_extra_time) {
+            $extra_hours = fake()->numberBetween(1, 4);
+            $total_amount = fake()->numberBetween(10, 1000);
+        }
         return [
-            // 'type' => fake()->randomElement(config('default_images')),
-            // 'base_amount',
-            // 'has_extra_time',
-            // 'extra_hours',
-            // 'total_amount',
+            'type' => fake()->randomElement([Attendance::ABSENT, Attendance::PRESENT]),
+            'has_extra_time' => $has_extra_time,
+            'extra_hours' => $extra_hours,
+            'total_amount' => $total_amount,
         ];
     }
 }
