c29ff0551af6894dc9fe4215462a36b0689283ee
mazimez <asif987patel@gmail.com>
2023-12-14 02:17:25 +0530

    added: some uypdate

--- resources/views/addEmployee.blade.php
+++ resources/views/addEmployee.blade.php
@@ -90,9 +90,55 @@
                         <td>{{ $employee->name }}</td>
                         <td>{{ $employee->contact_no }}</td>
                         <td>{{ $employee->salary_per_day }}</td>
-                        <td><button class="btn btn-warning" data-toggle="modal" data-target="#editModal1"><i
+                        <td><button class="btn btn-warning" data-toggle="modal" data-target="{{ "#editModal".$employee->id }}" ><i
                                     class="fas fa-edit"></i></button></td>
                     </tr>
+
+                     <!-- Edit Modal (You may need to create similar modals for each row) -->
+                    <div class="modal fade" id="{{ "editModal".$employee->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Edit Employee Salary</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <!-- Add form or content to edit the salary -->
+                                <form action="" method="POST">
+                                    @csrf
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Name</label>
+                                        <input type="text" class="form-control" id="name" value="{{ $employee->name }}"
+                                            placeholder="" name="edited_salary">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="contact" class="form-label">Contact No</label>
+                                        <input type="number" class="form-control" id="editedSalary"
+                                            placeholder=""  value="{{ $employee->contact_no }}" name="edited_salary">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="salary" class="form-label">Salary Per Hour</label>
+                                        <input type="number" class="form-control" id="salary"
+                                            placeholder=""  value="{{ $employee->salary_per_day }}" name="salary_per_hour">
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary">Save Changes</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
                 @endforeach
 
                 <!-- Add more rows as needed -->
@@ -101,48 +147,7 @@
         </table>
     </div>
 
-    <!-- Edit Modal (You may need to create similar modals for each row) -->
-    <div class="modal fade" id="editModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Employee Salary</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <!-- Add form or content to edit the salary -->
-                    <form action="" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="name" class="form-label">Name</label>
-                            <input type="text" class="form-control" id="name"
-                                placeholder="" name="edited_salary">
-                        </div>
 
-                        <div class="mb-3">
-                            <label for="contact" class="form-label">Contact No</label>
-                            <input type="number" class="form-control" id="editedSalary"
-                                placeholder="" name="edited_salary">
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="salary" class="form-label">Salary Per Hour</label>
-                            <input type="number" class="form-control" id="salary"
-                                placeholder="" name="salary_per_hour">
-                        </div>
-
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Save Changes</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <!-- Add more edit modals as needed -->
 
